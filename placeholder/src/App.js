9748539fc41c8a28e06d3ce314ee98f9c7e6f418
Your Name <you@example.com>
2023-03-07 17:19:25 -0600

    Officially importing the components, everying rendering

--- placeholder/src/App.js
+++ placeholder/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./App.css";
-// Navbar
-// Mission
-// Services
-// Gallery
-// Estimate Form
-// Contact
+import Navbar from "../src/Navbar/Navbar";
+import Mission from "../src/Mission/Mission";
+import Services from "../src/Services/Services";
+import Gallery from "../src/Gallery/Gallery";
+import Estimate from "../src/Estimate/Estimate";
+import Contact from "../src/Contact/Contact";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Mission /> */}
-      {/* <Services /> */}
-      {/* <Gallery /> */}
-      {/* <Estimate Form /> */}
-      {/* <Contact /> */}
+      <Navbar />
+      <Mission />
+      <Services />
+      <Gallery />
+      <Estimate />
+      <Contact />
     </>
   );
 }
