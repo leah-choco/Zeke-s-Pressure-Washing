ae29ffb8dd043d9052a7897ba0d40185e1712fc4
Your Name <you@example.com>
2023-03-09 14:31:16 -0600

    Setting up Navbar links

--- placeholder/src/Navbar/Navbar.jsx
+++ placeholder/src/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='navbar__links'>
+        <p><a href='#home'>Home</a></p>
+        <p><a href='#mission'>Our Mission</a></p>
+        <p><a href='#services'>Our Services</a></p>
+        <p><a href='#gallery'>Gallery</a></p>
+        <p><a href='#estimate'>Free Estimate</a></p>
+        <p><a href='#contact'>Contact Us</a></p>
+
+      </div>
+    </div>
   )
 }
 
