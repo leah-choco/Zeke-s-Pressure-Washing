dc2e418521ef8db5cae7bfc0fe3a48f03b139637
Your Name <you@example.com>
2023-03-09 14:57:12 -0600

    Discarding divs and using nav

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,16 +4,14 @@ import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar__links'>
-        <a href='#home'>Home</a>
-        <a href='#mission'>Our Mission</a>
-        <a href='#services'>Our Services</a>
-        <a href='#gallery'>Gallery</a>
-        <a href='#estimate'>Free Estimate</a>
-        <a href='#contact'>Contact Us</a>
-      </div>
-    </div>
+    <nav>
+      <a href='#home'>Home</a>
+      <a href='#mission'>Our Mission</a>
+      <a href='#services'>Our Services</a>
+      <a href='#gallery'>Gallery</a>
+      <a href='#estimate'>Free Estimate</a>
+      <a href='#contact'>Contact Us</a>
+    </nav>
   )
 }
 
