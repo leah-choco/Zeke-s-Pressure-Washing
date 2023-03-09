ff74abd5a3f660898bebd56b3d4efb3488f799bb
Your Name <you@example.com>
2023-03-09 15:20:47 -0600

    Pseudo coding out the different services

--- src/Services/Services.jsx
+++ src/Services/Services.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import './services.css';
 
+// Import photos of different services
+
+// Need to add a component here to be duplicated for each service
 
 const Services = () => {
   return (
@@ -10,7 +13,14 @@ const Services = () => {
         <h1>Our Services</h1>
       </div>
 
-      
+      {/* <div className='services__container'>
+        <article imgUrl={} text='Concrete' />
+        <article imgUrl={} text='Roof Softwash' />
+        <article imgUrl={} text='Woodwork' />
+        <article imgUrl={} text='Gutters' />
+        <article imgUrl={} text='House Wash' />
+
+      </div> */}
     </div>
   )
 }
