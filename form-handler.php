5a8ac4a40ddb3084f1fa79705c3d391c8e3d2ad0
Your Name <you@example.com>
2023-05-23 16:58:39 -0500

    Adding more code for the form handler

--- form-handler.php
+++ form-handler.php
@@ -5,6 +5,23 @@ $visitor_phone = $_POST['phone'];
 $visitor_address = $_POST['address'];
 $message = $_POST['message'];
 
+$email_from = 'info@zekespressurewashing.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".  
+                "User Phone: $visitor_phone.\n".
+                "User Address: $visitor_address.\n".
+                "User Message: $message.\n";
+
+$to = 'mrsleahchoco@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
 
 
 ?>
